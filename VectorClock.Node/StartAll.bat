82fd963ef498599c08cee7ae020eaf4400c4e630
Julius Hackel <Julius Hackel>
2016-06-07 18:51:07 +0200

    Added path (bin\Debug) to startall.bat

--- VectorClock.Node/StartAll.bat
+++ VectorClock.Node/StartAll.bat
@@ -1,5 +1,5 @@
 ﻿@echo off
 
-start VectorClock.Node.exe 0 1337
-start VectorClock.Node.exe 1 1338
-start VectorClock.Node.exe 2 1339
+start bin\Debug\VectorClock.Node.exe 0 1337
+start bin\Debug\VectorClock.Node.exe 1 1338
+start bin\Debug\VectorClock.Node.exe 2 1339
