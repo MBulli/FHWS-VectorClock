e3ebb1ab5b64b929b9457089e2dca5b3a0a039f1
MBulli <markusbullmann@gmail.com>
2016-06-08 15:42:59 +0200

    Added AutoscrollTextBox

---
+++ VectorClock.Commander/View/AutoScrollTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VectorClock.Commander.View
+{
+    class AutoScrollTextBox : TextBox
+    {
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            this.ScrollToEnd();
+        }
+    }
+}
